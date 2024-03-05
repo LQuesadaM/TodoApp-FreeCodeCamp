64c3d66278409762aeca2a3836c4deb183f00991
LQuesadaM <luisquesada163@gmail.com>
2024-03-05 16:16:16 -0300

    🚧 toggle buttom

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () => {
+    taskForm.classList.toggle("hidden");
+});
