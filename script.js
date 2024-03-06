e548ce31608f4deaeba9737c484ce4c4f4f801c8
LQuesadaM <luisquesada163@gmail.com>
2024-03-06 09:23:01 -0300

    ⚡️ enhance code readability and maintainability
    refactoring the submit event listener into two separate functions. The first function can be used to add the input values to taskData, while the second function can be responsible for adding the tasks to the DOM.

--- script.js
+++ script.js
@@ -13,6 +13,35 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  updateTaskContainer();
+  reset();
+};
+
+const updateTaskContainer = () => {
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+      <div class="task" id="${id}">
+        <p><strong>Title:</strong> ${title}</p>
+        <p><strong>Date:</strong> ${date}</p>
+        <p><strong>Description:</strong> ${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+      </div>
+    `;
+  });
+};
+
 const reset = () => {
   titleInput.value = "";
   dateInput.value = "";
@@ -44,26 +73,5 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-  const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
-    date: dateInput.value,
-    description: descriptionInput.value,
-  };
-  if (dataArrIndex === -1) {
-    taskData.unshift(taskObj);
-  }
-  taskData.forEach(({ id, title, date, description }) => {
-    tasksContainer.innerHTML += `
-      <div class="task" id="${id}">
-        <p><strong>Title:</strong> ${title}</p>
-        <p><strong>Date:</strong> ${date}</p>
-        <p><strong>Description:</strong> ${description}</p>
-        <button type="button" class="btn">Edit</button>
-        <button type="button" class="btn">Delete</button>
-      </div>
-    `;
-  });
-  reset();
+  addOrUpdateTask();
 });
