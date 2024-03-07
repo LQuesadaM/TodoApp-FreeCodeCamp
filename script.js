6088c4accfa229121cfed5a325f8a13493075041
LQuesadaM <luisquesada163@gmail.com>
2024-03-07 07:50:25 -0300

    🔨 🚧 adding functionality to the delete button

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
 };
 
 const reset = () => {
